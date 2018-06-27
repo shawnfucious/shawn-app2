891b00e34041798ff76c5665208eacd245a93598
Shawn <shawn@albersmail.com>
2018-06-26 22:28:56 -0400

    added initial maven project files

---
+++ src/main/java/com/albersoft/sample/shawnapp2/App.java
@@ -0,0 +1,13 @@
+package com.albersoft.sample.shawnapp2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
